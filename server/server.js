9a4233969661ab3885670c3e7321fd75d046667e
Pradeep Khanal <pradeepkhanal642@gmail.com>
2024-07-18 11:04:51 +1000

    server.hs file refined a little with some useful comments before deploying

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//serving the static files from the frontend build dist folder
 app.use(express.static("../client/dist"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//HTML routes
 require("./routes/htmlRoutes")(app);
+
+// Handling  other route in case
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
